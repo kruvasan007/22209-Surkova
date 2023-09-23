2f2e4409ca0d942af160beffb945f29f51f0f594
a.surkova <a.surkova@g.nsu.ru>
2023-09-23 19:39:02 +0700

    fix test

--- Lab2/test.cpp
+++ Lab2/test.cpp
@@ -1,3 +1,27 @@
-//
-// Created by Anastasia on 22.09.2023.
-//
+#include <gtest/gtest.h>
+#include "GameManager.h"
+#include "googletest/googlemock/include/gmock/gmock.h"
+#include "googletest/googletest/include/gtest/gtest.h"
+
+struct MyClassTest : public testing::Test {
+    GameManager *gameManager;
+    std::string inputName = "input.txt";
+    std::string outputName = "output.txt";
+
+    void SetUp() {
+        gameManager = new GameManager(inputName, outputName);
+    }
+
+    void TearDown() {
+        delete gameManager;
+    }
+};
+
+TEST_F(MyClassTest, Subtest_offlineGame) {
+    gameManager->startOfflineGame(1000);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
